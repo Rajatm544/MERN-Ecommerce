69403d484106c7bc50cf2cac42c60a19b4dda20a
Rajatm544 <rajatm544@gmail.com>
2021-06-30 01:30:39 +0530

    setup ES module syntax and env variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
 
+dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
@@ -16,4 +18,9 @@ app.get('/api/products/:id', (req, res) => {
 	res.json(product);
 });
 
-app.listen(5000, () => console.log('Server on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+	)
+);
