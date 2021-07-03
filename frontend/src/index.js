f56c810b30707c7e78366d4f3bc5f6d7dad187fc
Rajatm544 <rajatm544@gmail.com>
2021-07-04 00:30:20 +0530

    setup the initial redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
+import store from './store';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<Provider store={store}>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Provider>,
 	document.getElementById('root')
 );
 
