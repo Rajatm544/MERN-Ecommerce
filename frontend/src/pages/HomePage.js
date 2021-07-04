7ee60e1205c8b3411b20b0de43e7d8fcf594a85d
Rajatm544 <rajatm544@gmail.com>
2021-07-04 20:39:53 +0530

    dispatch list products action to get application state

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,30 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product.js';
 import { Row, Col } from 'react-bootstrap';
+import { listProducts } from '../actions/productActions';
+
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
 const HomePage = () => {
-	const [products, setProducts] = useState([]);
+	const dispatch = useDispatch();
+
+	const productList = useSelector((state) => state.productList);
+	const { products, loading, error } = productList;
 
 	useEffect(() => {
-		axios
-			.get('/api/products')
-			.then((res) => setProducts([...res.data]))
-			.catch((err) => console.error('Error', err));
-	}, []);
+		dispatch(listProducts());
+	}, [dispatch]);
 
 	return (
 		<>
 			<h1>Latest Products.</h1>
-			<Row>
-				{products.map((product) => {
-					return (
-						<Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-							<Product product={product} />
-						</Col>
-					);
-				})}
-			</Row>
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message variant='danger'>{error}</Message>
+			) : products ? (
+				<Row>
+					{products.map((product) => {
+						return (
+							<Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+								<Product product={product} />
+							</Col>
+						);
+					})}
+				</Row>
+			) : (
+				''
+			)}
 		</>
 	);
 };
