12ad7a95b70630cb0b6c61b7199b0d808a7ab864
Rajatm544 <rajatm544@gmail.com>
2021-07-15 00:06:17 +0530

    add a middleware in mongoDB model for user, to hash password before saving

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,18 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// encrypt password before saving
+userSchema.pre('save', async function (next) {
+	const user = this;
+	if (!user.isModified('password')) {
+		return next();
+	}
+	const salt = bcrypt.genSaltSync(10);
+	const hash = bcrypt.hashSync(user.password, salt);
+	user.password = hash;
+	next();
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
