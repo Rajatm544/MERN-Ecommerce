ff882faa1c78f334db0a6d6f200292d6abd4b716
Rajatm544 <rajatm544@gmail.com>
2021-07-16 23:36:15 +0530

    add reducers to handle user login

---
+++ frontend/src/reducers/userReducers.js
@@ -0,0 +1,21 @@
+import {
+	USER_LOGIN_FAILURE,
+	USER_LOGIN_REQUEST,
+	USER_LOGIN_SUCCESS,
+	USER_LOGOUT,
+} from '../constants/userConstants';
+
+export const userLoginReducer = (state = {}, action) => {
+	switch (action.type) {
+		case USER_LOGIN_REQUEST:
+			return { loading: true, userInfo: action.payload };
+		case USER_LOGIN_SUCCESS:
+			return { loading: false, userInfo: action.payload };
+		case USER_LOGIN_FAILURE:
+			return { loading: false, error: action.payload };
+		case USER_LOGOUT:
+			return {};
+		default:
+			return { ...state };
+	}
+};
