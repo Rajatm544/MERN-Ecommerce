385a46ae48e52958ca028dd3fc1b9cc2365900e0
Rajatm544 <rajatm544@gmail.com>
2021-07-20 23:39:47 +0530

    hide the alert messages after some time

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Alert } from 'react-bootstrap';
 
 const Message = ({ variant, children }) => {
-	return (
-		<Alert variant={variant}>
-			{/* <Alert.Heading>Oh snap!</Alert.Heading> */}
-			{children}
-		</Alert>
-	);
+	const [visible, setVisible] = useState(true);
+
+	useEffect(() => {
+		if (variant === 'danger') {
+			setTimeout(() => setVisible(false), 10000);
+		} else {
+			setTimeout(() => setVisible(false), 5000);
+		}
+	});
+
+	if (visible) {
+		return (
+			<Alert variant={variant} dismissable>
+				{/* <Alert.Heading>Oh snap!</Alert.Heading> */}
+				{children}
+			</Alert>
+		);
+	} else return null;
 };
 
 Message.defaultProps = {
