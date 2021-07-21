8371a409ada76bfd7f51626cfb35e86d629c23f6
Rajatm544 <rajatm544@gmail.com>
2021-07-22 01:51:45 +0530

    add constants and reducer to update user profile details

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,12 +9,15 @@ import {
 	USER_DETAILS_FAILURE,
 	USER_DETAILS_SUCCESS,
 	USER_DETAILS_REQUEST,
+	USER_PROFILE_UPDATE_REQUEST,
+	USER_PROFILE_UPDATE_SUCCESS,
+	USER_PROFILE_UPDATE_FAILURE,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
 	switch (action.type) {
 		case USER_LOGIN_REQUEST:
-			return { loading: true };
+			return { ...state, loading: true };
 		case USER_LOGIN_SUCCESS:
 			return { loading: false, userInfo: action.payload };
 		case USER_LOGIN_FAILURE:
@@ -29,7 +32,7 @@ export const userLoginReducer = (state = {}, action) => {
 export const userRegisterReducer = (state = {}, action) => {
 	switch (action.type) {
 		case USER_REGISTER_REQUEST:
-			return { loading: true };
+			return { ...state, loading: true };
 		case USER_REGISTER_SUCCESS:
 			return { loading: false, userInfo: action.payload };
 		case USER_REGISTER_FAILURE:
@@ -42,10 +45,22 @@ export const userRegisterReducer = (state = {}, action) => {
 export const userDetailsReducer = (state = { user: {} }, action) => {
 	switch (action.type) {
 		case USER_DETAILS_REQUEST:
-			return { loading: true };
+			return { ...state, loading: true };
 		case USER_DETAILS_SUCCESS:
 			return { loading: false, user: action.payload };
 		case USER_DETAILS_FAILURE:
+			return { loading: false, error: action.payload };
+		default:
+			return { ...state };
+	}
+};
+export const userProfileUpdateReducer = (state = {}, action) => {
+	switch (action.type) {
+		case USER_PROFILE_UPDATE_REQUEST:
+			return { loading: true };
+		case USER_PROFILE_UPDATE_SUCCESS:
+			return { loading: false, success: true, userInfo: action.payload };
+		case USER_PROFILE_UPDATE_FAILURE:
 			return { loading: false, error: action.payload };
 		default:
 			return { ...state };
