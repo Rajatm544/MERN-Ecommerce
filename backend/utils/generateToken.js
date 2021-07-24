e7603274a5374e5aa429c431420ae797e9a6ae78
Rajatm544 <rajatm544@gmail.com>
2021-07-24 23:08:56 +0530

    change the generateToken util to generate both the access and the refresh tokens

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,9 +1,15 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken = (id) => {
-	return jwt.sign({ id }, process.env.JWT_TOKEN_SECRET, {
-		expiresIn: '12h',
-	});
+const generateToken = (id, option) => {
+	if (option === 'access') {
+		return jwt.sign({ id }, process.env.JWT_ACCESS_TOKEN_SECRET, {
+			expiresIn: 60 * 30,
+		});
+	} else if (option === 'refresh') {
+		return jwt.sign({ id }, process.env.JWT_REFRESH_TOKEN_SECRET, {
+			expiresIn: '1d',
+		});
+	}
 };
 
 export default generateToken;
