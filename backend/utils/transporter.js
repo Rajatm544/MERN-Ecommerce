b7ff972150d59fb7da07e6cb235636dbbff7b0ff
Rajatm544 <rajatm544@gmail.com>
2021-07-26 01:10:31 +0530

    add a util file to set up the transporter for the nodemailer

---
+++ backend/utils/transporter.js
@@ -0,0 +1,17 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		type: 'OAuth2',
+		user: process.env.MAIL_USERNAME,
+		pass: process.env.MAIL_PASSWORD,
+		clientId: process.env.OAUTH_CLIENT_ID,
+		clientSecret: process.env.OAUTH_CLIENT_SECRET,
+		refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+	},
+});
+
+export default transporter;
