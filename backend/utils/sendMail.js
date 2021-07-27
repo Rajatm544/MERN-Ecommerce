748befa4816290537ad4473465d53d07a26697a4
Rajatm544 <rajatm544@gmail.com>
2021-07-27 20:27:41 +0530

    add option to send mail for resetting password

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -4,31 +4,67 @@ import generateToken from '../utils/generateToken.js';
 
 dotenv.config();
 
-const sendMail = async (id, email) => {
-	// create a new JWT to verify user via email
-	const emailToken = generateToken(id, 'email');
-	const url = `http://localhost:3000/user/confirm/${emailToken}`;
-	const mailOptions = {
-		from: process.env.EMAIL, // sender address
-		to: email,
-		subject: 'Confirm your email for Kosells', // Subject line
-		html: `<p>
+const sendMail = async (id, email, option) => {
+	if (option === 'email verification') {
+		// create a new JWT to verify user via email
+		const emailToken = generateToken(id, 'email');
+		const url = `http://localhost:3000/user/confirm/${emailToken}`;
+		const mailOptions = {
+			from: process.env.EMAIL, // sender address
+			to: email,
+			subject: 'Confirm your email for Kosells', // Subject line
+			html: `<p>
 					Click this link to 
 					<a href="${url}">verify your account</a>
 				</p>
 				
 			`,
-	};
+		};
 
-	const mailSent = await transporter.sendMail(mailOptions, (err, info) => {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log(info);
-		}
-	});
+		const mailSent = await transporter.sendMail(
+			mailOptions,
+			(err, info) => {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(info);
+				}
+			}
+		);
 
-	if (mailSent) return Promise.resolve(1);
+		if (mailSent) return Promise.resolve(1);
+	} else if (option === 'forgot password') {
+		// create a new JWT to verify user via email
+		const forgetPasswordToken = generateToken(id, 'forgot password');
+		const url = `http://localhost:3000/user/password/reset/${forgetPasswordToken}`;
+		const mailOptions = {
+			from: process.env.EMAIL, // sender address
+			to: email,
+			subject: 'Reset Password for Kosells', // Subject line
+			html: `<p>
+					Forgot your password? No worries! Just click this link to 
+					<a href="${url}">reset your password</a>. 
+					
+					
+					Note that this link is valid for only the next 10 minutes. 
+				</p>
+				
+			`,
+		};
+
+		const mailSent = await transporter.sendMail(
+			mailOptions,
+			(err, info) => {
+				if (err) {
+					console.log(err);
+				} else {
+					console.log(info);
+				}
+			}
+		);
+
+		if (mailSent) return Promise.resolve(1);
+	}
 };
 
 export default sendMail;
