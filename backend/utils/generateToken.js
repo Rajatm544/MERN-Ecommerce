4af7f52ea08dfcd66320a9f2fa3e53d434cb6df1
Rajatm544 <rajatm544@gmail.com>
2021-07-27 20:27:12 +0530

    generate jwt for resetting password

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -11,6 +11,10 @@ const generateToken = (id, option) => {
 		});
 	} else if (option === 'email') {
 		return jwt.sign({ id }, process.env.JWT_EMAIL_TOKEN_SECRET, {
+			expiresIn: 60 * 15,
+		});
+	} else if (option === 'forgot password') {
+		return jwt.sign({ id }, process.env.JWT_FORGOT_PASSWORD_TOKEN_SECRET, {
 			expiresIn: 60 * 10,
 		});
 	}
