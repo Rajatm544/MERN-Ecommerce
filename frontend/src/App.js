d6f9d81542239a1f0eea632e9551ac9eed3de4d9
Rajatm544 <rajatm544@gmail.com>
2021-07-28 00:49:09 +0530

    add route to handle resetting of password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
 import ConfirmPage from './pages/ConfirmPage';
+import PasswordResetPage from './pages/PasswordResetPage';
 
 const App = () => {
 	return (
@@ -21,6 +22,10 @@ const App = () => {
 					<Route path='/' component={HomePage} exact />
 					<Route path='/login' component={LoginPage} />
 					<Route path='/register' component={RegisterPage} />
+					<Route
+						path='/user/password/reset/:token'
+						component={PasswordResetPage}
+					/>
 					<Route path='/profile' component={ProfilePage} />
 					<Route path='/product/:id' component={ProductPage} />
 					<Route path='/cart/:id?' component={CartPage} />
