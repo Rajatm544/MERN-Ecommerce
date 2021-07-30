4f0b30862d513b434a097105b3cc6a7d1961f6cc
Rajatm544 <rajatm544@gmail.com>
2021-07-30 22:46:43 +0530

    fix bug with TTL of token object in mongoDB

--- backend/models/tokenModel.js
+++ backend/models/tokenModel.js
@@ -1,20 +1,22 @@
 import mongoose from 'mongoose';
 
-const tokenSchema = mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
+const tokenSchema = mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+		},
+		token: {
+			type: String,
+			required: true,
+		},
+		// expire_at: { type: Date, default: Date.now, expires: 1000 * 60 * 60 },
 	},
-	token: {
-		type: String,
-		required: true,
-	},
-	expire_at: {
-		type: Date,
-		default: Date.now,
-		expires: 60 * 60 * 24 * 7,
-	},
-});
+	{ timestamps: true }
+);
+
+tokenSchema.index({ createdAt: 1 }, { expires: '7d' });
+// console.log(tokenSchema.indexes());
 
 const Token = mongoose.model('Token', tokenSchema);
 
