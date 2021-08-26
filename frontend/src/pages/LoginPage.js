1c5bfe60f39c5f3e57f546ffe57fc55140fe13e6
Rajatm544 <rajatm544@gmail.com>
2021-08-26 18:21:39 +0530

    display options to login via google account, rewrite the action calling mechanism to avoid errors

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Button, InputGroup, Row, Col, Card } from 'react-bootstrap';
+import { loginUser } from '../actions/userActions';
+import { USER_LOGIN_SUCCESS } from '../constants/userConstants';
+import axios from 'axios';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import FormContainer from '../components/FormContainer';
-import { loginUser } from '../actions/userActions';
-import axios from 'axios';
+import SocialLoginOptions from '../components/SocialLoginOptions';
 
 const LoginPage = ({ location, history }) => {
 	const [showRedirectMsg, setShowRedirectMsg] = useState(false);
@@ -22,6 +24,44 @@ const LoginPage = ({ location, history }) => {
 	const userLogin = useSelector((state) => state.userLogin);
 	const { loading, userInfo, error } = userLogin;
 	const storedInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+	useEffect(() => {
+		// check for url params
+		if (window.location.search) {
+			const queries = window.location.search.split('&');
+			const isSuccess = queries[0].split('=')[1] === 'success';
+			const id = queries[1].split('=')[1];
+			if (isSuccess) {
+				// get user data and dispatch login success
+				axios
+					.post('/api/users/passport/data', {
+						id,
+					})
+					.then(({ data }) => {
+						const { id, email, name, isAdmin, isConfirmed } = data;
+						const userData = {
+							id,
+							email,
+							name,
+							isAdmin,
+							isConfirmed,
+							isSocialLogin: true,
+						};
+						dispatch({
+							type: USER_LOGIN_SUCCESS,
+							payload: userData,
+						});
+						// localStorage.setItem('refreshToken', data.refreshToken);
+						localStorage.setItem(
+							'userInfo',
+							JSON.stringify(userData)
+						);
+						localStorage.removeItem('promptEmailVerfication');
+						history.push('/');
+					});
+			}
+		}
+	}, [dispatch]);
 
 	useEffect(() => {
 		if (
@@ -87,146 +127,160 @@ const LoginPage = ({ location, history }) => {
 
 	if (!forgotPassword) {
 		return (
-			<FormContainer>
-				<h1>Sign In</h1>
-				{error && <Message variant='danger'>{error}</Message>}
-				{showRedirectMsg && (
-					<Message variant='info'>
-						Your session has expired. Please login again.
-					</Message>
-				)}
-				{loading ? (
-					<Loader />
-				) : (
-					<Form onSubmit={handleSubmit}>
-						<Form.Group controlId='email'>
-							<Form.Label>Email Address</Form.Label>
-							<Form.Control
-								size='lg'
-								placeholder='Enter Email Address'
-								type='email'
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-							/>
-						</Form.Group>
-						<Form.Group className='my-1'>
-							<Form.Label>Password</Form.Label>
-							<InputGroup className='mb-3'>
-								<Form.Control
-									size='lg'
-									type={type}
-									placeholder='Enter your password'
-									value={password}
-									style={{ borderRight: 'none' }}
-									onChange={(e) =>
-										setPassword(e.target.value)
-									}></Form.Control>
-								<InputGroup.Text
-									id='basic-addon2'
-									onClick={showHide}
+			<>
+				<FormContainer>
+					<h1>Sign In</h1>
+					{error && <Message variant='danger'>{error}</Message>}
+					{showRedirectMsg && (
+						<Message variant='info'>
+							Your session has expired. Please login again.
+						</Message>
+					)}
+					{loading ? (
+						<Loader />
+					) : (
+						<>
+							<Form
+								onSubmit={handleSubmit}
+								style={{ width: '33em' }}>
+								<Form.Group controlId='email'>
+									<Form.Label>Email Address</Form.Label>
+									<Form.Control
+										size='lg'
+										placeholder='Enter Email Address'
+										type='email'
+										value={email}
+										onChange={(e) =>
+											setEmail(e.target.value)
+										}
+									/>
+								</Form.Group>
+								<Form.Group className='my-1'>
+									<Form.Label>Password</Form.Label>
+									<InputGroup className='mb-3'>
+										<Form.Control
+											size='lg'
+											type={type}
+											placeholder='Enter your password'
+											value={password}
+											style={{ borderRight: 'none' }}
+											onChange={(e) =>
+												setPassword(e.target.value)
+											}></Form.Control>
+										<InputGroup.Text
+											id='basic-addon2'
+											onClick={showHide}
+											style={{
+												background: 'transparent',
+												borderLeft: 'none',
+												padding: '0.5em 0.5em 0.5em 0',
+											}}>
+											{type === 'text' ? (
+												<i className='far fa-eye-slash'></i>
+											) : (
+												<i className='far fa-eye'></i>
+											)}
+										</InputGroup.Text>
+									</InputGroup>
+								</Form.Group>
+								<Col
 									style={{
-										background: 'transparent',
-										borderLeft: 'none',
-										padding: '0.5em 0.5em 0.5em 0',
+										display: 'flex',
 									}}>
-									{type === 'text' ? (
-										<i className='far fa-eye-slash'></i>
-									) : (
-										<i className='far fa-eye'></i>
-									)}
-								</InputGroup.Text>
-							</InputGroup>
-						</Form.Group>
-						<Col
-							style={{
-								display: 'flex',
-							}}>
-							<Button
-								type='submit'
-								variant='dark'
-								className='my-1'>
-								Login
-							</Button>
-							<Button
-								type='button'
-								variant='link'
-								className='ms-auto'
-								size='sm'
-								onClick={() => setForgotPassword(true)}>
-								{/* <Link to='/user/password/reset'> */}
-								Forgot Password?
-								{/* </Link> */}
-							</Button>
-						</Col>
-					</Form>
-				)}
-				<Row>
-					<Col style={{ fontSize: '1.1em' }}>
-						New Here?{' '}
-						<Link
-							to={
-								redirect
-									? `/register?redirect=${redirect}`
-									: '/register'
-							}>
-							Register
-						</Link>
-					</Col>
-				</Row>
-			</FormContainer>
+									<Button
+										type='submit'
+										variant='dark'
+										className='my-1'>
+										Login
+									</Button>
+									<Button
+										type='button'
+										variant='link'
+										className='ms-auto'
+										size='sm'
+										onClick={() => setForgotPassword(true)}>
+										{/* <Link to='/user/password/reset'> */}
+										Forgot Password?
+										{/* </Link> */}
+									</Button>
+								</Col>
+							</Form>
+							<Row>
+								<Col style={{ fontSize: '1.1em' }}>
+									New Here?{' '}
+									<Link
+										to={
+											redirect
+												? `/register?redirect=${redirect}`
+												: '/register'
+										}>
+										Register
+									</Link>
+								</Col>
+							</Row>
+							<SocialLoginOptions />
+						</>
+					)}
+				</FormContainer>
+			</>
 		);
 	} else {
 		return (
-			<FormContainer>
-				<h1>Reset Password</h1>
-				{showLoading ? (
-					<Loader />
-				) : emailSent ? (
-					<Card>
-						<Card.Body className='pl-0'>
-							<Card.Title>Email Sent</Card.Title>
-							<Card.Text>
-								Please use the link sent to your email and reset
-								your password right away!
-							</Card.Text>
-						</Card.Body>
-					</Card>
-				) : (
-					<Form onSubmit={handleEmailSubmit}>
-						<Form.Group controlId='email'>
-							<Form.Label>Email Address</Form.Label>
-							<Form.Control
-								size='lg'
-								placeholder='Enter Email Address'
-								type='email'
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-							/>
-						</Form.Group>
-						<Col
-							style={{
-								display: 'flex',
-							}}>
-							<Button
-								type='submit'
-								variant='dark'
-								className='mt-3'>
-								Submit
-							</Button>
-							<Button
-								type='button'
-								variant='link'
-								className='ms-auto'
-								size='sm'
-								onClick={() => setForgotPassword(false)}>
-								{/* <Link to='/user/password/reset'> */}I
-								remember my password
-								{/* </Link> */}
-							</Button>
-						</Col>
-					</Form>
-				)}
-			</FormContainer>
+			<>
+				<FormContainer>
+					<h1>Reset Password</h1>
+					{showLoading ? (
+						<Loader />
+					) : emailSent ? (
+						<Card>
+							<Card.Body className='pl-0'>
+								<Card.Title>Email Sent</Card.Title>
+								<Card.Text>
+									Please use the link sent to your email and
+									reset your password right away!
+								</Card.Text>
+							</Card.Body>
+						</Card>
+					) : (
+						<Form
+							onSubmit={handleEmailSubmit}
+							style={{ width: '33em' }}>
+							<Form.Group controlId='email'>
+								<Form.Label>Email Address</Form.Label>
+								<Form.Control
+									size='lg'
+									placeholder='Enter Email Address'
+									type='email'
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+								/>
+							</Form.Group>
+							<Col
+								style={{
+									display: 'flex',
+								}}>
+								<Button
+									type='submit'
+									variant='dark'
+									className='mt-3'>
+									Submit
+								</Button>
+								<Button
+									type='button'
+									variant='link'
+									className='ms-auto'
+									size='sm'
+									onClick={() => setForgotPassword(false)}>
+									{/* <Link to='/user/password/reset'> */}I
+									remember my password
+									{/* </Link> */}
+								</Button>
+							</Col>
+						</Form>
+					)}
+					<SocialLoginOptions />
+				</FormContainer>
+			</>
 		);
 	}
 };
