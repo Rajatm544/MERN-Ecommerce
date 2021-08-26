c76218489d2491c5c4807800fe0c98f7a2b0fd64
Rajatm544 <rajatm544@gmail.com>
2021-08-26 22:26:32 +0530

    setup passport strategy for github login

--- backend/config/passportSetup.js
+++ backend/config/passportSetup.js
@@ -1,6 +1,7 @@
 import passport from 'passport';
 import dotenv from 'dotenv';
 import GoogleStrategy from 'passport-google-oauth20';
+import GithubStrategy from 'passport-github2';
 import User from '../models/userModel.js';
 
 dotenv.config();
@@ -32,6 +33,35 @@ const setupPassport = () => {
 							isAdmin: false,
 							isConfirmed: profile._json.email_verified,
 							googleID: profile.id,
+							email: profile._json.email,
+						}).then((user) => {
+							// console.log('User created ', user);
+							done(null, user);
+						});
+					} else {
+						done(null, foundUser);
+						// console.log('User exists ', foundUser);
+					}
+				});
+			}
+		)
+	);
+
+	passport.use(
+		new GithubStrategy(
+			{
+				clientID: process.env.GITHUB_CLIENT_ID,
+				clientSecret: process.env.GITHUB_CLIENT_SECRET,
+				callbackURL: 'http://localhost:5000/api/auth/github/redirect',
+			},
+			(accessToken, refreshToken, profile, done) => {
+				User.findOne({ githubID: profile.id }).then((foundUser) => {
+					if (!foundUser) {
+						User.create({
+							name: profile.displayName,
+							isAdmin: false,
+							isConfirmed: !!profile._json.email,
+							githubID: profile.id,
 							email: profile._json.email,
 						}).then((user) => {
 							// console.log('User created ', user);
