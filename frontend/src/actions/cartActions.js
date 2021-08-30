a830ec7c8fd05cdd9e397af38161f13366d3bad3
Rajatm544 <rajatm544@gmail.com>
2021-08-30 20:36:09 +0530

    add cart action to save payment method option

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,6 +3,7 @@ import {
 	CART_ADD_ITEM,
 	CART_REMOVE_ITEM,
 	CART_SAVE_SHIPPING_ADDRESS,
+	CART_SAVE_PAYMENT_METHOD,
 } from '../constants/cartConstants';
 
 export const addItem = (id, qty) => async (dispatch, getState) => {
@@ -50,7 +51,18 @@ export const saveShippingAddress = (data) => async (dispatch) => {
 			type: CART_SAVE_SHIPPING_ADDRESS,
 			payload: data,
 		});
-		localStorage.setItem('cartItems', JSON.stringify(data));
+		localStorage.setItem('shippingAddress', JSON.stringify(data));
+	} catch (error) {
+		console.log(error);
+	}
+};
+export const savePaymentMethod = (data) => async (dispatch) => {
+	try {
+		dispatch({
+			type: CART_SAVE_PAYMENT_METHOD,
+			payload: data,
+		});
+		localStorage.setItem('paymentMethod', JSON.stringify(data));
 	} catch (error) {
 		console.log(error);
 	}
