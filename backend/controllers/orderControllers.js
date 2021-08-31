9ccf679b87f47dbb5e9b04388af9cc493caa92dc
Rajatm544 <rajatm544@gmail.com>
2021-08-31 14:29:18 +0530

    add controllers to add order items in server

---
+++ backend/controllers/orderControllers.js
@@ -0,0 +1,38 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/productModel.js';
+
+// @desc  create a new order
+// @route GET /api/orders
+// @access PRIVATE
+const addOrderItems = asyncHandler(async (req, res) => {
+	const {
+		orderItems,
+		shippingAddress,
+		paymentMethod,
+		itemsPrice,
+		shippingPrice,
+		taxPrice,
+		totalPrice,
+	} = req.body;
+
+	if (orderItems && !orderItems.length) {
+		res.status(401);
+		throw new Error('No order items');
+	} else {
+		const order = new Order({
+			orderItems,
+			user: req.user._id,
+			shippingAddress,
+			paymentMethod,
+			itemsPrice,
+			shippingPrice,
+			taxPrice,
+			totalPrice,
+		});
+
+		const createdOrder = await order.save();
+		res.status(201).json(createdOrder);
+	}
+});
+
+export { addOrderItems };
