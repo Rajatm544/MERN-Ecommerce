7d1bcb7d04233d12f748f8a5edf12d4a74d340da
Rajatm544 <rajatm544@gmail.com>
2021-09-01 19:57:15 +0530

    alter Message component to not disapper after a while

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -4,13 +4,13 @@ import { Alert } from 'react-bootstrap';
 const Message = ({ variant, children }) => {
 	const [visible, setVisible] = useState(true);
 
-	useEffect(() => {
-		if (variant === 'danger' || variant === 'info') {
-			setTimeout(() => setVisible(false), 10000);
-		} else {
-			setTimeout(() => setVisible(false), 8000);
-		}
-	});
+	// useEffect(() => {
+	// 	if (variant === 'danger' || variant === 'info') {
+	// 		setTimeout(() => setVisible(false), 10000);
+	// 	} else {
+	// 		setTimeout(() => setVisible(false), 8000);
+	// 	}
+	// });
 
 	if (visible) {
 		return <Alert variant={variant}>{children}</Alert>;
