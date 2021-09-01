c9465ef5a906928b34b7431f1cc067dc0cde7f69
Rajatm544 <rajatm544@gmail.com>
2021-09-01 17:50:34 +0530

    fix bug with useeffect in place order page and display loader on submit

--- frontend/src/pages/PlaceOrderPage.js
+++ frontend/src/pages/PlaceOrderPage.js
@@ -4,6 +4,7 @@ import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CheckoutStatus from '../components/CheckoutStatus';
 import Message from '../components/Message';
+import Loader from '../components/Loader';
 import { createOrder } from '../actions/orderActions';
 
 const PlaceOrderPage = ({ history }) => {
@@ -15,10 +16,11 @@ const PlaceOrderPage = ({ history }) => {
 	const { order, loading, success, error } = orderCreate;
 
 	useEffect(() => {
-		if (order && success) {
+		if (success) {
 			history.push(`/order/${order._id}`);
 		}
-	}, [order, success, history]);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [success, history]);
 
 	// All prices
 	cart.itemsPrice = cartItems
@@ -50,112 +52,126 @@ const PlaceOrderPage = ({ history }) => {
 		<>
 			<CheckoutStatus step1 step2 step3 step4 />
 			<Row>
-				<Col md={8}>
-					<ListGroup variant='flush'>
-						<ListGroup.Item>
-							<h2>Shipping</h2>
-							<p>
-								<strong>Address: </strong>{' '}
-								{shippingAddress.address},{' '}
-								{shippingAddress.city}-
-								{shippingAddress.postalCode},{' '}
-								{shippingAddress.country}
-							</p>
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<h2>Payment Method</h2>
-							<p>
-								<strong>Method: </strong> {paymentMethod}
-							</p>
-						</ListGroup.Item>
-						<ListGroup.Item>
-							<h2>Cart Items</h2>
-							{cartItems.length !== 0 ? (
-								<ListGroup variant='flush'>
-									<div style={{ background: 'red' }}></div>
-									{cartItems.map((item, idx) => (
-										<ListGroup.Item key={idx}>
-											<Row>
-												<Col md={2}>
-													<Image
-														src={item.image}
-														alt={item.name}
-														fluid
-														rounded
-													/>
-												</Col>
-												<Col>
-													<Link
-														to={`/product/${item.product}`}>
-														{item.name}
-													</Link>
-												</Col>
-												<Col md={4}>
-													{item.qty} x {item.price} ={' '}
-													{(
-														item.qty * item.price
-													).toFixed(2)}
-												</Col>
-											</Row>
-										</ListGroup.Item>
-									))}
-								</ListGroup>
-							) : (
-								<Message variant='info'>
-									Your cart is empty
-								</Message>
-							)}
-						</ListGroup.Item>
-					</ListGroup>
-				</Col>
-				<Col md={4}>
-					<Card>
-						<ListGroup variant='flush'>
-							<ListGroup.Item>
-								<h2>Order Summary</h2>
-							</ListGroup.Item>
-							<ListGroup.Item>
-								<Row>
-									<Col>Items</Col>
-									<Col>$ {cart.itemsPrice}</Col>
-								</Row>
-							</ListGroup.Item>
-							<ListGroup.Item>
-								<Row>
-									<Col>Shipping</Col>
-									<Col>$ {cart.shippingPrice}</Col>
-								</Row>
-							</ListGroup.Item>
-							<ListGroup.Item>
-								<Row>
-									<Col>Tax</Col>
-									<Col>$ {cart.taxPrice}</Col>
-								</Row>
-							</ListGroup.Item>
-							<ListGroup.Item>
-								<Row>
-									<Col>Total</Col>
-									<Col>$ {cart.totalPrice}</Col>
-								</Row>
-							</ListGroup.Item>
-							{error && (
+				{loading ? (
+					<Loader />
+				) : (
+					<>
+						<Col md={8}>
+							<ListGroup variant='flush'>
 								<ListGroup.Item>
-									<Message variant='danger'>{error}</Message>
+									<h2>Shipping</h2>
+									<p>
+										<strong>Address: </strong>{' '}
+										{shippingAddress.address},{' '}
+										{shippingAddress.city}-
+										{shippingAddress.postalCode},{' '}
+										{shippingAddress.country}
+									</p>
 								</ListGroup.Item>
-							)}
-							<ListGroup.Item className='d-grid gap-2'>
-								<Button
-									variant='dark'
-									type='button'
-									className='btn-block'
-									disabled={!cartItems.length}
-									onClick={handleOrder}>
-									Place Order
-								</Button>
-							</ListGroup.Item>
-						</ListGroup>
-					</Card>
-				</Col>
+								<ListGroup.Item>
+									<h2>Payment Method</h2>
+									<p>
+										<strong>Method: </strong>{' '}
+										{paymentMethod}
+									</p>
+								</ListGroup.Item>
+								<ListGroup.Item>
+									<h2>Cart Items</h2>
+									{cartItems.length !== 0 ? (
+										<ListGroup variant='flush'>
+											<div
+												style={{
+													background: 'red',
+												}}></div>
+											{cartItems.map((item, idx) => (
+												<ListGroup.Item key={idx}>
+													<Row>
+														<Col md={2}>
+															<Image
+																src={item.image}
+																alt={item.name}
+																fluid
+																rounded
+															/>
+														</Col>
+														<Col>
+															<Link
+																to={`/product/${item.product}`}>
+																{item.name}
+															</Link>
+														</Col>
+														<Col md={4}>
+															{item.qty} x{' '}
+															{item.price} ={' '}
+															{(
+																item.qty *
+																item.price
+															).toFixed(2)}
+														</Col>
+													</Row>
+												</ListGroup.Item>
+											))}
+										</ListGroup>
+									) : (
+										<Message variant='info'>
+											Your cart is empty
+										</Message>
+									)}
+								</ListGroup.Item>
+							</ListGroup>
+						</Col>
+						<Col md={4}>
+							<Card>
+								<ListGroup variant='flush'>
+									<ListGroup.Item>
+										<h2>Order Summary</h2>
+									</ListGroup.Item>
+									<ListGroup.Item>
+										<Row>
+											<Col>Items</Col>
+											<Col>$ {cart.itemsPrice}</Col>
+										</Row>
+									</ListGroup.Item>
+									<ListGroup.Item>
+										<Row>
+											<Col>Shipping</Col>
+											<Col>$ {cart.shippingPrice}</Col>
+										</Row>
+									</ListGroup.Item>
+									<ListGroup.Item>
+										<Row>
+											<Col>Tax</Col>
+											<Col>$ {cart.taxPrice}</Col>
+										</Row>
+									</ListGroup.Item>
+									<ListGroup.Item>
+										<Row>
+											<Col>Total</Col>
+											<Col>$ {cart.totalPrice}</Col>
+										</Row>
+									</ListGroup.Item>
+									{error && (
+										<ListGroup.Item>
+											<Message variant='danger'>
+												{error}
+											</Message>
+										</ListGroup.Item>
+									)}
+									<ListGroup.Item className='d-grid gap-2'>
+										<Button
+											variant='dark'
+											type='button'
+											className='btn-block'
+											disabled={!cartItems.length}
+											onClick={handleOrder}>
+											Place Order
+										</Button>
+									</ListGroup.Item>
+								</ListGroup>
+							</Card>
+						</Col>
+					</>
+				)}
 			</Row>
 		</>
 	);
