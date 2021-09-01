af8c456301bd4fe5ab9880d0a66b283bba16aa65
Rajatm544 <rajatm544@gmail.com>
2021-09-01 18:00:28 +0530

    add order action to fetch an order by id

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,9 @@ import {
 	ORDER_CREATE_REQUEST,
 	ORDER_CREATE_SUCCESS,
 	ORDER_CREATE_FAILURE,
+	ORDER_DETAILS_REQUEST,
+	ORDER_DETAILS_SUCCESS,
+	ORDER_DETAILS_FAILURE,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -17,6 +20,30 @@ export const orderCreateReducer = (state = {}, action) => {
 				order: action.payload,
 			};
 		case ORDER_CREATE_FAILURE:
+			return {
+				loading: false,
+				error: action.payload,
+			};
+		default:
+			return { ...state };
+	}
+};
+export const orderDetailsReducer = (
+	state = { orderItems: [], shippingAddress: {} },
+	action
+) => {
+	switch (action.type) {
+		case ORDER_DETAILS_REQUEST:
+			return {
+				...state,
+				loading: true,
+			};
+		case ORDER_DETAILS_SUCCESS:
+			return {
+				loading: false,
+				order: action.payload,
+			};
+		case ORDER_DETAILS_FAILURE:
 			return {
 				loading: false,
 				error: action.payload,
