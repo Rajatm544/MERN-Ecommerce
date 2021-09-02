91a56508c375d24f432502e50e92f387397cf36f
Rajatm544 <rajatm544@gmail.com>
2021-09-02 20:10:39 +0530

    add controller to send the paypal client id

---
+++ backend/controllers/configControllers.js
@@ -0,0 +1,10 @@
+import asyncHandler from 'express-async-handler';
+
+// @desc fetch PAYPAL client id credential
+// @route GET /api/config/paypal
+// @access PRIVATE
+const getPaypalClientId = asyncHandler(async (req, res) => {
+	res.send(process.env.PAYPAL_CLIENT_ID);
+});
+
+export { getPaypalClientId };
