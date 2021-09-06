a2d1d3256f984507da11278a3e3a2a0823b74537
Rajatm544 <rajatm544@gmail.com>
2021-09-06 20:37:26 +0530

    add actions to update the user details from admin pamel

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -31,6 +31,9 @@ import {
 	USER_DELETE_REQUEST,
 	USER_DELETE_SUCCESS,
 	USER_DELETE_FAILURE,
+	USER_UPDATE_REQUEST,
+	USER_UPDATE_SUCCESS,
+	USER_UPDATE_FAILURE,
 } from '../constants/userConstants';
 import { ORDER_USER_LIST_RESET } from '../constants/orderConstants';
 import axios from 'axios';
@@ -393,6 +396,47 @@ export const deleteUser = (id) => async (dispatch, getState) => {
 	} catch (error) {
 		dispatch({
 			type: USER_DELETE_FAILURE,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
+
+export const updateUser = (user) => async (dispatch, getState) => {
+	try {
+		dispatch({ type: USER_UPDATE_REQUEST });
+
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		const config = userInfo.isSocialLogin
+			? {
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `SocialLogin ${userInfo.id}`,
+					},
+			  }
+			: {
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${userInfo.accessToken}`,
+					},
+			  };
+
+		const { data } = await axios.put(
+			`/api/users/${user._id}`,
+			user,
+			config
+		);
+
+		dispatch({ type: USER_UPDATE_SUCCESS });
+		dispatch({ type: USER_DETAILS_SUCCESS, payload: data });
+	} catch (error) {
+		dispatch({
+			type: USER_UPDATE_FAILURE,
 			payload:
 				error.response && error.response.data.message
 					? error.response.data.message
