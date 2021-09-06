e7518b3e977dcfb48a3b317f91c98a3c05976b46
Rajatm544 <rajatm544@gmail.com>
2021-09-06 19:48:44 +0530

    remove unwanted variable to avoid warnings

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -387,7 +387,7 @@ export const deleteUser = (id) => async (dispatch, getState) => {
 					},
 			  };
 
-		const { data } = await axios.delete(`/api/users/${id}`, config);
+		await axios.delete(`/api/users/${id}`, config);
 
 		dispatch({ type: USER_DELETE_SUCCESS });
 	} catch (error) {
