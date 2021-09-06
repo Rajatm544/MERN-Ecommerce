04bdf72e8064a0804aad92a6f81e2b2510446903
Rajatm544 <rajatm544@gmail.com>
2021-09-06 17:29:03 +0530

    increase the access token valid duration to 1 hour

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const generateToken = (id, option) => {
 	if (option === 'access') {
 		return jwt.sign({ id }, process.env.JWT_ACCESS_TOKEN_SECRET, {
-			expiresIn: 60 * 30,
+			expiresIn: 60 * 60,
 		});
 	} else if (option === 'refresh') {
 		return jwt.sign({ id }, process.env.JWT_REFRESH_TOKEN_SECRET, {
