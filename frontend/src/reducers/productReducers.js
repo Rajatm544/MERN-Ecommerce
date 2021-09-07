8de682b6855bae19a0fa41770ffafbd55390c1c8
Rajatm544 <rajatm544@gmail.com>
2021-09-07 19:46:09 +0530

    add reducer to update the product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -12,6 +12,10 @@ import {
 	PRODUCT_CREATE_FAILURE,
 	PRODUCT_CREATE_REQUEST,
 	PRODUCT_CREATE_RESET,
+	PRODUCT_UPDATE_SUCCESS,
+	PRODUCT_UPDATE_FAILURE,
+	PRODUCT_UPDATE_REQUEST,
+	PRODUCT_UPDATE_RESET,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -69,6 +73,21 @@ export const productCreateReducer = (state = {}, action) => {
 			return { loading: false, error: action.payload };
 		case PRODUCT_CREATE_RESET:
 			return {};
+		default:
+			return { ...state };
+	}
+};
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+	switch (action.type) {
+		case PRODUCT_UPDATE_REQUEST:
+			return { loading: true };
+		case PRODUCT_UPDATE_SUCCESS:
+			return { loading: false, success: true, product: action.payload };
+		case PRODUCT_UPDATE_FAILURE:
+			return { loading: false, error: action.payload };
+		case PRODUCT_UPDATE_RESET:
+			return { product: {} };
 		default:
 			return { ...state };
 	}
