ceacd4b99ea3d9edd05ff7c314d81580544d1c9b
Rajatm544 <rajatm544@gmail.com>
2021-09-07 15:40:23 +0530

    add API endpoints to create a new product and to edit it

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,22 +3,28 @@ import {
 	deleteProduct,
 	getAllProducts,
 	getProductById,
+	createProduct,
+	updateProduct,
 } from '../controllers/productControllers.js';
 import { protectRoute, isAdmin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-// @desc fetch all the products
+// @desc fetch all the products, create a product
 // @route GET /api/products
 // @access PUBLIC
-router.route('/').get(getAllProducts);
+router
+	.route('/')
+	.get(getAllProducts)
+	.post(protectRoute, isAdmin, createProduct);
 
-// @desc Fetch a single product by id, Delete a product
+// @desc Fetch a single product by id, Delete a product,  update a product
 // @route GET /api/products/:id
 // @access PUBLIC & PRIVATE/ADMIN
 router
 	.route('/:id')
 	.get(getProductById)
-	.delete(protectRoute, isAdmin, deleteProduct);
+	.delete(protectRoute, isAdmin, deleteProduct)
+	.put(protectRoute, isAdmin, updateProduct);
 
 export default router;
