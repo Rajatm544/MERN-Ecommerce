9b351fb09a5934f12fdd08bc5de0236c9d25db3d
Rajatm544 <rajatm544@gmail.com>
2021-09-07 15:39:50 +0530

    add product controllers to create a new product and to edit it

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -37,4 +37,62 @@ const deleteProduct = asyncHandler(async (req, res) => {
 	}
 });
 
-export { getProductById, getAllProducts, deleteProduct };
+// @desc Create a product
+// @route POST /api/products/
+// @access PRIVATE/ADMIN
+const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: 'Sample',
+		brand: 'Sample Brand',
+		category: 'Sample Category',
+		numReviews: 0,
+		countInStock: 0,
+		price: 0,
+		user: req.user._id,
+		image: '/images/alexa.jpg',
+		description: 'Sample description',
+	});
+	const createdProduct = await product.save();
+	res.status(201).json(createdProduct);
+});
+
+// @desc Update a product
+// @route PUT /api/products/:id
+// @access PRIVATE/ADMIN
+const updateProduct = asyncHandler(async (req, res) => {
+	const {
+		name,
+		price,
+		brand,
+		category,
+		numReviews,
+		countInStock,
+		description,
+		image,
+	} = req.body;
+	const product = await Product.findById(req.params.id);
+	if (product) {
+		if (name) product.name = name;
+		if (price) product.price = price;
+		if (brand) product.brand = brand;
+		if (category) product.category = category;
+		if (numReviews) product.numReviews = numReviews;
+		if (countInStock) product.countInStock = countInStock;
+		if (description) product.description = description;
+		if (image) product.image = image;
+
+		const updatedProduct = await product.save();
+		if (updatedProduct) res.status(201).json(updatedProduct);
+	} else {
+		res.status(404);
+		throw new Error('Product not available');
+	}
+});
+
+export {
+	getProductById,
+	getAllProducts,
+	deleteProduct,
+	createProduct,
+	updateProduct,
+};
