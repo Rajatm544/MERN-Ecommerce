f098cb1cdff0213eb463104849f4d7f7cba328db
Rajatm544 <rajatm544@gmail.com>
2021-09-08 23:50:15 +0530

    add order action to update order isDelivered

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -8,6 +8,9 @@ import {
 	ORDER_PAY_REQUEST,
 	ORDER_PAY_SUCCESS,
 	ORDER_PAY_FAILURE,
+	ORDER_DELIVER_REQUEST,
+	ORDER_DELIVER_SUCCESS,
+	ORDER_DELIVER_FAILURE,
 	ORDER_USER_LIST_REQUEST,
 	ORDER_USER_LIST_SUCCESS,
 	ORDER_USER_LIST_FAILURE,
@@ -128,6 +131,44 @@ export const payOrder =
 			});
 		}
 	};
+
+export const deliverOrder = (orderID) => async (dispatch, getState) => {
+	try {
+		dispatch({ type: ORDER_DELIVER_REQUEST });
+
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		const config = userInfo.isSocialLogin
+			? {
+					headers: {
+						Authorization: `SocialLogin ${userInfo.id}`,
+					},
+			  }
+			: {
+					headers: {
+						Authorization: `Bearer ${userInfo.accessToken}`,
+					},
+			  };
+
+		const { data } = await axios.put(
+			`/api/orders/${orderID}/deliver`,
+			{},
+			config
+		);
+
+		dispatch({ type: ORDER_DELIVER_SUCCESS, payload: data });
+	} catch (error) {
+		dispatch({
+			type: ORDER_DELIVER_FAILURE,
+			payload:
+				error.response && error.response.data.message
+					? error.response.data.message
+					: error.message,
+		});
+	}
+};
 
 export const listMyOrders = () => async (dispatch, getState) => {
 	try {
