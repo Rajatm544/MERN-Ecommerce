9b8c636eca9f02ca8657d7a42b9558add1264d81
Rajatm544 <rajatm544@gmail.com>
2021-09-14 23:26:48 +0530

    remove text being a required prop in the Ratings component

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -30,7 +30,7 @@ Rating.defaultProps = {
 
 Rating.propTypes = {
 	value: PropTypes.number.isRequired,
-	text: PropTypes.string.isRequired,
+	text: PropTypes.string,
 	color: PropTypes.string,
 };
 
