3683e793907c92713e24f7e1edcf44b493352ecf
Rajatm544 <rajatm544@gmail.com>
2021-09-17 00:25:31 +0530

    alter product action to fetch all products to filter by keyword as well

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -20,23 +20,27 @@ import {
 } from '../constants/productConstants';
 import axios from 'axios';
 
-export const listProducts = () => async (dispatch) => {
-	try {
-		dispatch({ type: PRODUCT_LIST_REQUEST });
+export const listProducts =
+	(keyword = '') =>
+	async (dispatch) => {
+		try {
+			dispatch({ type: PRODUCT_LIST_REQUEST });
 
-		const { data } = await axios.get('/api/products');
+			const { data } = await axios.get(
+				`/api/products?keyword=${keyword}`
+			);
 
-		dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
-	} catch (error) {
-		dispatch({
-			type: PRODUCT_LIST_FAILURE,
-			payload:
-				error.response && error.response.data.message
-					? error.response.data.message
-					: error.message,
-		});
-	}
-};
+			dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+		} catch (error) {
+			dispatch({
+				type: PRODUCT_LIST_FAILURE,
+				payload:
+					error.response && error.response.data.message
+						? error.response.data.message
+						: error.message,
+			});
+		}
+	};
 
 export const listProductDetails = (id) => async (dispatch) => {
 	try {
