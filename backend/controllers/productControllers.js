57151ec1e9e8263d5eae11f84bdfcfd0b3d9fb26
Rajatm544 <rajatm544@gmail.com>
2021-09-17 00:24:52 +0530

    add an option to return selective products using keyword in the product controller to get pproducts

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -5,7 +5,15 @@ import Product from '../models/productModel.js';
 // @route GET /api/products
 // @access PUBLIC
 const getAllProducts = asyncHandler(async (req, res) => {
-	const products = await Product.find({});
+	const keyword = req.query.keyword
+		? {
+				name: {
+					$regex: req.query.keyword,
+					$options: 'i',
+				},
+		  }
+		: {};
+	const products = await Product.find({ ...keyword });
 	res.json(products);
 });
 
