0c24c2a2f415417e981eea9d30c8fc9939aaf40c
Rajatm544 <rajatm544@gmail.com>
2021-09-22 19:04:18 +0530

    add an image magnifier component

---
+++ frontend/src/components/ImageMagnifier.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { Image } from 'react-bootstrap';
+
+const ImageMagnifier = ({
+	src,
+	alt,
+	title,
+	width,
+	height,
+	magnifierHeight = 200,
+	magnifieWidth = 200,
+	zoomLevel = 1.5,
+}) => {
+	const [[x, y], setXY] = useState([0, 0]);
+	const [[imgWidth, imgHeight], setSize] = useState([0, 0]);
+	const [showMagnifier, setShowMagnifier] = useState(false);
+	return (
+		<div
+			style={{
+				position: 'relative',
+				height: height,
+				width: width,
+			}}>
+			<Image
+				src={src}
+				style={{ height: height, width: width }}
+				fluid
+				title={title}
+				onMouseEnter={(e) => {
+					// update image size and turn-on magnifier
+					const elem = e.currentTarget;
+					const { width, height } = elem.getBoundingClientRect();
+					setSize([width, height]);
+					setShowMagnifier(true);
+				}}
+				onMouseMove={(e) => {
+					// update cursor position
+					const elem = e.currentTarget;
+					const { top, left } = elem.getBoundingClientRect();
+
+					// calculate cursor position on the image
+					const x = e.pageX - left - window.pageXOffset;
+					const y = e.pageY - top - window.pageYOffset;
+					setXY([x, y]);
+				}}
+				onMouseLeave={() => {
+					// close magnifier
+					setShowMagnifier(false);
+				}}
+				alt={alt}
+			/>
+
+			<div
+				style={{
+					display: showMagnifier ? '' : 'none',
+					position: 'absolute',
+					// prevent maginier blocks the mousemove event of img
+					pointerEvents: 'none',
+					// set size of magnifier
+					height: `${magnifierHeight}px`,
+					width: `${magnifieWidth}px`,
+					// move element center to cursor pos
+					top: `${y - magnifierHeight / 2}px`,
+					left: `${x - magnifieWidth / 2}px`,
+					opacity: '1', // reduce opacity so you can verify position
+					border: '1px solid lightgray',
+					backgroundColor: 'white',
+					backgroundImage: `url('${src}')`,
+					backgroundRepeat: 'no-repeat',
+					borderRadius: '50%',
+
+					//calculate zoomed image size
+					backgroundSize: `${imgWidth * zoomLevel}px ${
+						imgHeight * zoomLevel
+					}px`,
+
+					//calculete position of zoomed image.
+					backgroundPositionX: `${
+						-x * zoomLevel + magnifieWidth / 2
+					}px`,
+					backgroundPositionY: `${
+						-y * zoomLevel + magnifierHeight / 2
+					}px`,
+				}}></div>
+		</div>
+	);
+};
+
+export default ImageMagnifier;
