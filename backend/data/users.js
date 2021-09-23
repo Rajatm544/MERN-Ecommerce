0684b12a665344f5e8e5dc92247b2d8916f89163
Rajatm544 <rajatm544@gmail.com>
2021-09-23 11:50:50 +0530

    add a default image for the identicon avatar for the seeded users data

--- backend/data/users.js
+++ backend/data/users.js
@@ -7,24 +7,28 @@ const users = [
 		password: bcrypt.hashSync('pass123', 12),
 		isAdmin: true,
 		isConfirmed: true,
+		avatar: '/images/icon_user.png',
 	},
 	{
 		name: 'Rajat',
 		email: 'rajat@brand.com',
 		password: bcrypt.hashSync('pass123', 12),
 		isConfirmed: true,
+		avatar: '/images/icon_user.png',
 	},
 	{
 		name: 'Ravi',
 		email: 'ravi@brand.com',
 		password: bcrypt.hashSync('pass123', 12),
 		isConfirmed: true,
+		avatar: '/images/icon_user.png',
 	},
 	{
 		name: 'Voca',
 		email: 'dataxom889@flmmo.com',
 		password: bcrypt.hashSync('pass123', 12),
 		isConfirmed: true,
+		avatar: '/images/icon_user.png',
 	},
 ];
 
