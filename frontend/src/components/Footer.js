01aea049f696433a9ff55ab93cb5daa29fb56e11
Rajatm544 <rajatm544@gmail.com>
2021-09-25 23:41:26 +0530

    change the footer design

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
 	return (
 		<footer>
 			<Container>
-				<Row>
-					<Col className='text-center py-2'>
-						Copyright &copy; Rajat
-					</Col>
-				</Row>
+				<div
+					style={{
+						display: 'flex',
+						justifyContent: 'flex-end',
+						alignItems: 'center',
+						marginRight: '-3em',
+						marginBottom: '1em',
+					}}>
+					<div className='footer-icons'>
+						<a
+							href='https://github.com/Rajatm544'
+							target='_blank'
+							rel='noopener noreferrer'>
+							<i className='fab fa-github footer-icon'></i>
+						</a>
+						<a
+							href='https://www.linkedin.com/in/rajat--m'
+							target='_blank'
+							rel='noopener noreferrer'>
+							<i className='fab fa-linkedin-in footer-icon'></i>
+						</a>
+						<a
+							href='https://twitter.com/Rajat__m'
+							target='_blank'
+							rel='noopener noreferrer'>
+							<i className='fab fa-twitter footer-icon'></i>
+						</a>
+						<a
+							href='https://rajatm.netlify.app/'
+							target='_blank'
+							rel='noopener noreferrer'>
+							<i className='fas fa-globe-asia footer-icon'></i>
+						</a>
+					</div>
+					<div className='footer-copyright'>&copy;2021 Kosells</div>
+				</div>
 			</Container>
 		</footer>
 	);
