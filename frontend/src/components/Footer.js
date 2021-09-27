73c932bc324b307cbf6c06f81685d9b22e51e377
Rajatm544 <rajatm544@gmail.com>
2021-09-28 01:04:39 +0530

    reduce icon size in footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Footer = () => {
 	return (
