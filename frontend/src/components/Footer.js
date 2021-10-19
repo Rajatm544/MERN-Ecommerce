818395f2f05144dca10be17d91653e0e908d6583
Rajatm544 <rajatm544@gmail.com>
2021-10-19 18:02:08 +0530

    fix footer style for responsiveness

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,46 +4,37 @@ import { Container } from 'react-bootstrap';
 
 const Footer = () => {
 	return (
-		<footer>
-			<Container>
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'flex-end',
-						alignItems: 'center',
-						marginRight: '-3em',
-						marginBottom: '1em',
-					}}>
-					<div className='footer-icons'>
-						<a
-							href='https://github.com/Rajatm544'
-							target='_blank'
-							rel='noopener noreferrer'>
-							<i className='fab fa-github footer-icon'></i>
-						</a>
-						<a
-							href='https://www.linkedin.com/in/rajat--m'
-							target='_blank'
-							rel='noopener noreferrer'>
-							<i className='fab fa-linkedin-in footer-icon'></i>
-						</a>
-						<a
-							href='https://twitter.com/Rajat__m'
-							target='_blank'
-							rel='noopener noreferrer'>
-							<i className='fab fa-twitter footer-icon'></i>
-						</a>
-						<a
-							href='https://rajatm.netlify.app/'
-							target='_blank'
-							rel='noopener noreferrer'>
-							<i className='fas fa-globe-asia footer-icon'></i>
-						</a>
-					</div>
-					<div className='footer-copyright'>&copy;2021 Kosells</div>
+		<Container>
+			<footer className='footer-container'>
+				<div className='footer-icons'>
+					<a
+						href='https://github.com/Rajatm544'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<i className='fab fa-github footer-icon'></i>
+					</a>
+					<a
+						href='https://www.linkedin.com/in/rajat--m'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<i className='fab fa-linkedin-in footer-icon'></i>
+					</a>
+					<a
+						href='https://twitter.com/Rajat__m'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<i className='fab fa-twitter footer-icon'></i>
+					</a>
+					<a
+						href='https://rajatm.netlify.app/'
+						target='_blank'
+						rel='noopener noreferrer'>
+						<i className='fas fa-globe-asia footer-icon'></i>
+					</a>
 				</div>
-			</Container>
-		</footer>
+				<div className='footer-copyright'>&copy;2021 Kosells</div>
+			</footer>
+		</Container>
 	);
 };
 
