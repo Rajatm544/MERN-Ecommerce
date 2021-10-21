5e58d36d59e658fcf3f9be5c09ab25dd24803639
Rajatm544 <rajatm544@gmail.com>
2021-10-21 22:19:13 +0530

    add aria labels to footer icons

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,24 +9,28 @@ const Footer = () => {
 				<div className='footer-icons'>
 					<a
 						href='https://github.com/Rajatm544'
+						aria-label='github account'
 						target='_blank'
 						rel='noopener noreferrer'>
 						<i className='fab fa-github footer-icon' />
 					</a>
 					<a
 						href='https://www.linkedin.com/in/rajat--m'
+						aria-label='linkedin account'
 						target='_blank'
 						rel='noopener noreferrer'>
 						<i className='fab fa-linkedin-in footer-icon' />
 					</a>
 					<a
 						href='https://twitter.com/Rajat__m'
+						aria-label='twitter account'
 						target='_blank'
 						rel='noopener noreferrer'>
 						<i className='fab fa-twitter footer-icon' />
 					</a>
 					<a
 						href='https://rajatm.netlify.app/'
+						aria-label='developer portfolio'
 						target='_blank'
 						rel='noopener noreferrer'>
 						<i className='fas fa-globe-asia footer-icon' />
