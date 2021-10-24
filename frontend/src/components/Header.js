f3ca079bba7a2a9760899c6875562dbc0e1d3a0e
Rajatm544 <rajatm544@gmail.com>
2021-10-24 23:16:42 +0530

    rewrite navbar to display differently on mobile screens

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -65,7 +65,7 @@ const Header = () => {
 				}}
 			/>
 
-			<Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+			<Navbar bg='primary' variant='dark' expand='lg'>
 				<Container>
 					<LinkContainer to='/'>
 						<Navbar.Brand>
@@ -74,107 +74,157 @@ const Header = () => {
 								className='nav-logo'
 								alt='logo'
 							/>
-							KOSELLS
+							Kosells
 						</Navbar.Brand>
 					</LinkContainer>
 
-					<Navbar.Toggle
+					{/* <Navbar.Toggle
 						className='navbar-toggle-btn'
 						aria-controls='basic-navbar-nav'
+					/> */}
+
+					{/* <Navbar.Collapse id='basic-navbar-nav'> */}
+					<Route
+						render={({ history }) => (
+							<div className='d-none d-md-block'>
+								<SearchBox history={history} />
+							</div>
+						)}
 					/>
 
-					<Navbar.Collapse id='basic-navbar-nav'>
-						<Route
-							render={({ history }) => (
-								<div className='d-none d-md-block'>
-									<SearchBox history={history} />
-								</div>
-							)}
-						/>
-
-						<Nav className='ms-auto'>
-							<LinkContainer to='/cart'>
-								<Nav.Link>
-									{count ? (
-										<div className='nav-cart-size'>
-											<span
-												style={
-													count > 10
-														? { fontSize: '0.6em' }
-														: { fontSize: '0.7em' }
-												}>
-												{count}
-											</span>
-										</div>
-									) : (
-										''
-									)}
-									<i
-										style={{ fontSize: '1.2em' }}
-										className='fas fa-shopping-cart'
-									/>{' '}
-									Cart
-								</Nav.Link>
-							</LinkContainer>
-							{userInfo && userInfo.isAdmin && (
-								<NavDropdown
-									title='Admin'
-									id='adminMenu'
-									show={show2}
-									onClick={handleDropdown2}>
-									<LinkContainer to='/admin/userlist'>
-										<NavDropdown.Item>
-											Users
-										</NavDropdown.Item>
-									</LinkContainer>
-									<LinkContainer to='/admin/productlist'>
-										<NavDropdown.Item>
-											Products
-										</NavDropdown.Item>
-									</LinkContainer>
-									<LinkContainer to='/admin/orderlist'>
-										<NavDropdown.Item>
-											Orders
-										</NavDropdown.Item>
-									</LinkContainer>
-								</NavDropdown>
-							)}
-							{userInfo ? (
-								<div className='nav-avatar-container'>
-									<img
-										src={userInfo.avatar}
-										className='nav-avatar'
-										alt={userInfo.name}
-									/>
-									<NavDropdown
-										title={userInfo.name}
-										id={userInfo.id}
-										show={show1}
-										onClick={handleDropdown1}>
-										<LinkContainer to='/profile'>
-											<NavDropdown.Item>
-												Profile
-											</NavDropdown.Item>
-										</LinkContainer>
-										<NavDropdown.Item
-											onClick={handleLogout}>
-											Logout
-										</NavDropdown.Item>
-									</NavDropdown>
-								</div>
-							) : (
-								<LinkContainer to='/login' variant='primary'>
+					<Nav
+						className='ms-auto nav-mobile'
+						style={
+							userInfo
+								? {
+										justifyContent: 'space-between',
+								  }
+								: {
+										justifyContent: 'space-evenly',
+								  }
+						}>
+						{userInfo && userInfo.isAdmin && (
+							<>
+								<LinkContainer
+									className='d-block d-md-none'
+									to='/admin/userlist'>
 									<Nav.Link>
-										<i
-											style={{ fontSize: '1.2em' }}
-											className='fas fa-user'
-										/>{' '}
-										Sign In
+										<i className='fas fa-users' />
 									</Nav.Link>
 								</LinkContainer>
-							)}
-						</Nav>
-					</Navbar.Collapse>
+								<LinkContainer
+									className='d-block d-md-none'
+									to='/admin/orderlist'>
+									<Nav.Link>
+										<i className='fas fa-user-shield' />
+									</Nav.Link>
+								</LinkContainer>
+								<LinkContainer
+									className='d-block d-md-none'
+									to='/admin/productlist'>
+									<Nav.Link>
+										<i className='fas fa-shopping-bag' />
+									</Nav.Link>
+								</LinkContainer>
+							</>
+						)}
+						<LinkContainer to='/cart'>
+							<Nav.Link>
+								{count ? (
+									<div className='nav-cart-size'>
+										Cart
+										<span
+											style={
+												count > 10
+													? { fontSize: '0.6em' }
+													: { fontSize: '0.7em' }
+											}>
+											{count}
+										</span>
+									</div>
+								) : (
+									''
+								)}
+								<i className='fas fa-shopping-cart navbar-icons' />{' '}
+								{!(userInfo && userInfo.isAdmin) ||
+								window.innerWidth >= 430
+									? 'Cart'
+									: ''}
+							</Nav.Link>
+						</LinkContainer>
+						{userInfo && userInfo.isAdmin && (
+							<NavDropdown
+								className='d-none d-md-block'
+								title='Admin'
+								id='adminMenu'
+								show={show2}
+								onClick={handleDropdown2}>
+								<LinkContainer to='/admin/userlist'>
+									<NavDropdown.Item>Users</NavDropdown.Item>
+								</LinkContainer>
+								<LinkContainer to='/admin/productlist'>
+									<NavDropdown.Item>
+										Products
+									</NavDropdown.Item>
+								</LinkContainer>
+								<LinkContainer to='/admin/orderlist'>
+									<NavDropdown.Item>Orders</NavDropdown.Item>
+								</LinkContainer>
+							</NavDropdown>
+						)}
+
+						{userInfo && (
+							<Nav.Link
+								className='d-block d-md-none'
+								onClick={handleLogout}>
+								<i className='fas fa-sign-out-alt navbar-icons' />{' '}
+								{!(userInfo && userInfo.isAdmin) && 'Logout'}
+							</Nav.Link>
+						)}
+						{userInfo ? (
+							<div className='nav-avatar-container'>
+								<LinkContainer
+									to='/profile'
+									className='d-block d-md-none'>
+									<Nav.Link>
+										<img
+											src={userInfo.avatar}
+											className='nav-avatar'
+											alt={userInfo.name}
+										/>
+									</Nav.Link>
+								</LinkContainer>
+								<img
+									src={userInfo.avatar}
+									className='nav-avatar d-none d-md-block'
+									alt={userInfo.name}
+								/>
+								<NavDropdown
+									className='d-none d-md-block'
+									title={userInfo.name}
+									id={userInfo.id}
+									show={show1}
+									onClick={handleDropdown1}>
+									<LinkContainer to='/profile'>
+										<NavDropdown.Item>
+											Profile
+										</NavDropdown.Item>
+									</LinkContainer>
+									<NavDropdown.Item onClick={handleLogout}>
+										Logout
+									</NavDropdown.Item>
+								</NavDropdown>
+							</div>
+						) : (
+							<LinkContainer to='/login' variant='primary'>
+								<Nav.Link>
+									<i className='fas fa-user navbar-icons' />{' '}
+									Sign In
+								</Nav.Link>
+							</LinkContainer>
+						)}
+					</Nav>
+					{/* </Navbar.Collapse> */}
 				</Container>
 			</Navbar>
 		</header>
