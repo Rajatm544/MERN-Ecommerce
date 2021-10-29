c9d16d811ad74545366f741d317fbbbcbc411cb5
Rajatm544 <rajatm544@gmail.com>
2021-10-29 23:43:12 +0530

    create a page for the 404 error

---
+++ frontend/src/pages/ErrorPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import errorImg from '../assets/404Error.png';
+import { Link } from 'react-router-dom';
+
+const ErrorPage = () => {
+	return (
+		<div className='text-center'>
+			<img className='error-img' alt='error' src={errorImg} />
+			<p className='mb-0 mt-3 text-error'>
+				<i className='far fa-frown' /> Looks like this page does not
+				exist.
+			</p>
+			<p className='mt-0 text-error'>
+				Go Back to the <Link to='/'>Home Page</Link>
+			</p>
+		</div>
+	);
+};
+
+export default ErrorPage;
