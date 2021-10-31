8665f6fcc18138dfe4286b2b09854e356d34c6b4
Rajatm544 <rajatm544@gmail.com>
2021-10-31 15:23:59 +0530

    remove Switch from the router in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
