e02250d59f7216dd8031d7cdfd2a13fb44f67dda
Rajatm544 <rajatm544@gmail.com>
2021-10-31 14:54:43 +0530

    fix bug with social login page not working

--- frontend/src/components/LoginOptions.js
+++ frontend/src/components/LoginOptions.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const LoginOptions = ({ match }) => {
-	const option = match.params.match;
+	const option = match.params.option;
 	useEffect(() => {
 		if (option) axios.get(`/api/auth/${option}`);
 	}, [option]);
