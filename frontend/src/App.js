4b458958150d6faf7e262139d1316b60285ec2e4
Rajatm544 <rajatm544@gmail.com>
2021-11-01 19:35:58 +0530

    add back the errorpage component along with Switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -21,85 +21,87 @@ import UserEditPage from './pages/UserEditPage';
 import ProductListPage from './pages/ProductListPage';
 import ProductEditPage from './pages/ProductEditPage';
 import OrderListPage from './pages/OrderListPage';
+import ErrorPage from './pages/ErrorPage';
 
 import ServiceWorkerWrapper from './ServiceWorkerWrapper';
-// import ErrorPage from './pages/ErrorPage';
-// import LoginOptions from './components/LoginOptions';
 
 const App = () => {
 	return (
 		<Router>
 			<Header />
 			<ServiceWorkerWrapper />
+
 			<main className='py-2'>
 				<Container>
-					{/* <Switch> */}
-					<Route path='/' component={HomePage} exact />
-					<Route path='/search/:keyword' component={HomePage} exact />
-					<Route
-						path='/page/:pageNumber'
-						component={HomePage}
-						exact
-					/>
-					<Route
-						path='/search/:keyword/page/:pageNumber'
-						exact
-						component={HomePage}
-					/>
-					<Route path='/login' component={LoginPage} />
-					<Route path='/register' component={RegisterPage} />
-					<Route
-						path='/user/password/reset/:token'
-						component={PasswordResetPage}
-					/>
-					<Route path='/profile' component={ProfilePage} />
-					<Route path='/product/:id' component={ProductPage} />
-					<Route path='/cart/:id?' component={CartPage} />
-					<Route
-						path='/user/confirm/:token'
-						component={ConfirmPage}
-						exact
-					/>
-					<Route path='/shipping' component={ShippingPage} />
-					<Route path='/payment' component={PaymentPage} />
-					<Route path='/placeorder' component={PlaceOrderPage} />
-					<Route path='/order/:id' component={OrderPage} />
-					<Route path='/admin/userlist' component={UserListPage} />
-					<Route
-						path='/admin/user/:id/edit'
-						component={UserEditPage}
-					/>
-					<Route
-						path='/admin/productlist'
-						exact
-						component={ProductListPage}
-					/>
-					<Route
-						path='/admin/productlist/:pageNumber'
-						component={ProductListPage}
-						exact
-					/>
-					<Route
-						path='/admin/product/:id/edit'
-						component={ProductEditPage}
-					/>
-					<Route
-						path='/admin/orderlist'
-						component={OrderListPage}
-						exact
-					/>
-					<Route
-						path='/admin/orderlist/:pageNumber'
-						component={OrderListPage}
-						exact
-					/>
-					{/* <Route
-							path='/api/auth/:option'
+					<Switch>
+						<Route path='/' component={HomePage} exact />
+						<Route
+							path='/search/:keyword'
+							component={HomePage}
 							exact
-							component={LoginOptions}
-						/> */}
-					{/* <Route component={ErrorPage} /> */}
-					{/* </Switch> */}
+						/>
+						<Route
+							path='/page/:pageNumber'
+							component={HomePage}
+							exact
+						/>
+						<Route
+							path='/search/:keyword/page/:pageNumber'
+							exact
+							component={HomePage}
+						/>
+						<Route path='/login' component={LoginPage} />
+						<Route path='/register' component={RegisterPage} />
+						<Route
+							path='/user/password/reset/:token'
+							component={PasswordResetPage}
+						/>
+						<Route path='/profile' component={ProfilePage} />
+						<Route path='/product/:id' component={ProductPage} />
+						<Route path='/cart/:id?' component={CartPage} />
+						<Route
+							path='/user/confirm/:token'
+							component={ConfirmPage}
+							exact
+						/>
+						<Route path='/shipping' component={ShippingPage} />
+						<Route path='/payment' component={PaymentPage} />
+						<Route path='/placeorder' component={PlaceOrderPage} />
+						<Route path='/order/:id' component={OrderPage} />
+						<Route
+							path='/admin/userlist'
+							component={UserListPage}
+						/>
+						<Route
+							path='/admin/user/:id/edit'
+							component={UserEditPage}
+						/>
+						<Route
+							path='/admin/productlist'
+							exact
+							component={ProductListPage}
+						/>
+						<Route
+							path='/admin/productlist/:pageNumber'
+							component={ProductListPage}
+							exact
+						/>
+						<Route
+							path='/admin/product/:id/edit'
+							component={ProductEditPage}
+						/>
+						<Route
+							path='/admin/orderlist'
+							component={OrderListPage}
+							exact
+						/>
+						<Route
+							path='/admin/orderlist/:pageNumber'
+							component={OrderListPage}
+							exact
+						/>
+						<Route component={ErrorPage} />
+					</Switch>
 				</Container>
 			</main>
 			<Footer />
