077a10f99859b1e421f68b5065b3d6c3079e654a
Rajatm544 <rajatm544@gmail.com>
2021-11-01 19:12:09 +0530

    blacklist routes with /api/auth from service worker caching

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -37,6 +37,10 @@ registerRoute(
 			return false;
 		} // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
+		if (url.pathname.indexOf('/api/auth') !== -1) {
+			return false;
+		}
+
 		if (url.pathname.match(fileExtensionRegexp)) {
 			return false;
 		} // Return true to signal that we want to use the handler.
