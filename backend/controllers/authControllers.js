9c04025d2155638dc98a699b910fac9ca66ec39e
Rajatm544 <rajatm544@gmail.com>
2021-11-02 23:50:01 +0530

    clean up the auth controller which handles success and failure redirects after passport login

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,79 +1,19 @@
 import asyncHandler from 'express-async-handler';
 import getAuthErrorCode from '../utils/getAuthErrorCode.js';
 
+// To redirect to some route caught by the react router in the frontend, after successfully logging in
 const frontendURL = process.env.FRONTEND_BASE_URL;
 
+// controller for the routes that handle the success redirects from all 4 passport strategies
 const passportLoginSuccess = asyncHandler(async (req, res) => {
 	res.redirect(`${frontendURL}/login?login=success&id=${req.user._id}`);
 });
 
+// controller for the routes that handle the failure redirects from all 4 passport strategies
 const passportLoginFailure = asyncHandler(async (req, res) => {
 	const errorMsg = req.flash('error')[0];
 	const errorCode = getAuthErrorCode(errorMsg);
 	res.redirect(`${frontendURL}/login?login=failed&errorCode=${errorCode}`);
 });
-
-// // @desc login user using the github strategy
-// // @route GET /api/auth/github/redirect/success
-// // @access PUBLIC
-// const githubLoginSuccess = asyncHandler(async (req, res) => {
-// 	// console.log(req.user);
-// 	// res.send('success');
-// 	res.redirect(`${frontendURL}/login?login=success&id=${req.user._id}`);
-// });
-
-// // @desc login user using the github strategy
-// // @route GET /api/auth/github/redirect/failure
-// // @access PUBLIC
-// const githubLoginFailure = asyncHandler(async (req, res) => {
-// 	const errorMsg = req.flash('error')[0];
-// 	const errorCode = getAuthErrorCode(errorMsg);
-// 	res.redirect(`${frontendURL}/login?login=failed&errorCode=${errorCode}`);
-// });
-
-// // @desc login user using the twitter strategy
-// // @route GET /api/auth/twitter/redirect/success
-// // @access PUBLIC
-// const twitterLoginSuccess = asyncHandler(async (req, res) => {
-// 	res.redirect(`${frontendURL}/login?login=success&id=${req.user._id}`);
-// });
-
-// // @desc login user using the twitter strategy
-// // @route GET /api/auth/twitter/redirect/failure
-// // @access PUBLIC
-// const twitterLoginFailure = asyncHandler(async (req, res) => {
-// 	const errorMsg = req.flash('error')[0];
-// 	const errorCode = getAuthErrorCode(errorMsg);
-
-// 	res.redirect(`${frontendURL}/login?login=failed&errorCode=${errorCode}`);
-// });
-
-// // @desc login user using the linkedin strategy
-// // @route GET /api/auth/linkedin/redirect/success
-// // @access PUBLIC
-// const linkedinLoginSuccess = asyncHandler(async (req, res) => {
-// 	res.redirect(`${frontendURL}/login?login=success&id=${req.user._id}`);
-// });
-
-// // @desc login user using the linkedin strategy
-// // @route GET /api/auth/linkedin/redirect/failure
-// // @access PUBLIC
-// const linkedinLoginFailure = asyncHandler(async (req, res) => {
-// 	const errorMsg = req.flash('error')[0];
-// 	const errorCode = getAuthErrorCode(errorMsg);
-
-// 	res.redirect(`${frontendURL}/login?login=failed&errorCode=${errorCode}`);
-// });
-
-// export {
-// 	googleLoginSuccess,
-// 	googleLoginFailure,
-// 	githubLoginSuccess,
-// 	githubLoginFailure,
-// 	twitterLoginSuccess,
-// 	twitterLoginFailure,
-// 	linkedinLoginSuccess,
-// 	linkedinLoginFailure,
-// };
 
 export { passportLoginSuccess, passportLoginFailure };
