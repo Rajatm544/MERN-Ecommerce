a36241fd21197f6111d7462f7c7ba3defecdc757
Rajatm544 <rajatm544@gmail.com>
2021-11-03 17:52:39 +0530

    add necessary comments to the generate token util function

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,21 +1,22 @@
 import jwt from 'jsonwebtoken';
 
+// generate a JWT token for the various applications represented by the 'option' argument
 const generateToken = (id, option) => {
 	if (option === 'access') {
 		return jwt.sign({ id }, process.env.JWT_ACCESS_TOKEN_SECRET, {
-			expiresIn: 60 * 60,
+			expiresIn: 60 * 60, // 1 hour
 		});
 	} else if (option === 'refresh') {
 		return jwt.sign({ id }, process.env.JWT_REFRESH_TOKEN_SECRET, {
-			expiresIn: '7d',
+			expiresIn: '7d', // 7 days
 		});
 	} else if (option === 'email') {
 		return jwt.sign({ id }, process.env.JWT_EMAIL_TOKEN_SECRET, {
-			expiresIn: 60 * 15,
+			expiresIn: 60 * 15, // 15 minutes
 		});
 	} else if (option === 'forgot password') {
 		return jwt.sign({ id }, process.env.JWT_FORGOT_PASSWORD_TOKEN_SECRET, {
-			expiresIn: 60 * 10,
+			expiresIn: 60 * 10, // 10 minutes
 		});
 	}
 };
