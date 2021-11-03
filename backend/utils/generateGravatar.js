9a483a7499343f470fa5f330452a3b2efe82e2c0
Rajatm544 <rajatm544@gmail.com>
2021-11-03 17:52:26 +0530

    add necessary comments to the generate gravatar util function

--- backend/utils/generateGravatar.js
+++ backend/utils/generateGravatar.js
@@ -1,11 +1,12 @@
 import gravatar from 'gravatar';
 
 const generateGravatar = (email) => {
+	// generate a url for the gravatar that is using https
 	const avatar = gravatar.url(email, {
 		protocol: 'https',
-		s: '200',
-		r: 'PG',
-		d: 'identicon',
+		s: '200', // size: 200x200
+		r: 'PG', // rating: PG
+		d: 'identicon', // default: identicon
 	});
 	return avatar;
 };
