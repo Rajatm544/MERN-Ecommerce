72800ae85858811b56da14c588566eb182d7a6b1
Rajatm544 <rajatm544@gmail.com>
2021-11-03 17:48:51 +0530

    add necessary comments to the server file

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import colors from 'colors';
-import morgan from 'morgan';
-import compression from 'compression';
-import cors from 'cors';
-import passport from 'passport';
-import passportSetup from './config/passportSetup.js';
-import cookieSession from 'cookie-session';
+import colors from 'colors'; // color the statements in the server side console log
+import morgan from 'morgan'; // show the API endpoints
+import compression from 'compression'; // use gzip compression in the express server
+import cors from 'cors'; // allow cross origin requests
+import passport from 'passport'; // for all social login options
+import cookieSession from 'cookie-session'; // for implementing cookie sessions for passport
+import flash from 'connect-flash'; // so that passport flash messages can work
 import path from 'path';
 
 // middleware
@@ -20,7 +20,6 @@ import orderRoutes from './routes/orderRoutes.js';
 import configRoutes from './routes/configRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import setupPassport from './config/passportSetup.js';
-import flash from 'connect-flash';
 
 dotenv.config();
 const app = express();
@@ -31,20 +30,19 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 // connect to the mongoDB database
 connectDB();
 
-// middleware to use req.body
-app.use(express.json());
-app.use(cors());
-app.use(compression());
+app.use(express.json()); // middleware to use req.body
+app.use(cors()); // to avoid CORS errors
+app.use(compression()); // to use gzip
 
 // use cookie sessions
 app.use(
 	cookieSession({
-		maxAge: 1000 * 60 * 60 * 24,
+		maxAge: 1000 * 60 * 60 * 24, // 1 day
 		keys: [process.env.COOKIE_SESSION_KEY],
 	})
 );
 
-// initialise passport middleware to use sessions
+// initialise passport middleware to use sessions, and flash messages
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
@@ -62,6 +60,7 @@ app.use('/api/upload', uploadRoutes);
 
 const __dirname = path.resolve();
 
+// To prepare for deployment
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/frontend/build')));
 
