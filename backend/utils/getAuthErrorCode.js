71b32605aca21054bc1424e630294138f6230b6a
Rajatm544 <rajatm544@gmail.com>
2021-11-03 17:53:00 +0530

    add necessary comments to the getauthcode util function

--- backend/utils/getAuthErrorCode.js
+++ backend/utils/getAuthErrorCode.js
@@ -1,4 +1,6 @@
+// This is used to send the correct error code to be sent to as the parameter after redirecting to frontend after failed passport login
 const getAuthErrorCode = (msg) => {
+	// This msg is obtained as the flash message from the passport login routes
 	if (msg === 'Registered using google account') return 0;
 	if (msg === 'Registered using github account') return 1;
 	if (msg === 'Registered using twitter account') return 2;
