c5d1185a5dee6505430dbc403bff37025476089f
Rajatm544 <rajatm544@gmail.com>
2021-11-21 22:36:03 +0530

    add a skeleton shimmer component for the loading animation

---
+++ frontend/src/components/SkeletonShimmer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SkeletonShimmer = () => {
+	return (
+		<div className='shimmer-wrapper'>
+			<div className='shimmer' />
+		</div>
+	);
+};
+
+export default SkeletonShimmer;
